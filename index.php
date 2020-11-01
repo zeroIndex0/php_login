34e32770d37c08a6130030296f9d23da0fc22f2c
zeroIndex0 <sagtogi@gmail.com>
2020-11-01 12:11:16 -0500

    added mock data for the button click

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
   <form method="POST">
     <input type="submit" name="test" value="button">
   </form>
-  <!-- This is no longer working, was just used as a test to check my db connection
+  <!-- Added mock data into the database, just using as a test to check my db connection -->
   <?php if (isset($_POST["test"])) {
     include_once "api/read.php";
-    echo $data_array[0]["name"];
-  } ?> -->
+    echo "Id: " . $data_array[0]["id"];
+    echo "<br>";
+    echo "Username: " . $data_array[0]["username"];
+  } ?>
 </body>
 
 </html>

7fa9f5c5b2b0b83ec13fe7c286384b4808a39b96
zeroIndex0 <sagtogi@gmail.com>
2020-11-05 16:15:58 -0500

    added extra rows that are currently not being used

--- usertable.sql
+++ usertable.sql
@@ -10,4 +10,10 @@ last_name VARCHAR(25),
 username VARCHAR(100),
 email VARCHAR(100),
 password VARCHAR(255),
+signup_date DATE,
+profile_pic VARCHAR(255),
+num_posts INT,
+num_likes INT,
+user_closed VARCHAR(3),
+friend_array TEXT
 );

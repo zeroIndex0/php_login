1c37b8757e4c93ca0a5c28e2f83f0985f515ec90
zeroIndex0 <sagtogi@gmail.com>
2020-11-16 11:30:53 -0500

    added show/hide for the login/register fields

--- register.php
+++ register.php
@@ -27,102 +27,125 @@ require "includes/form_handlers/login_handler.php";
 
 
           <!-- Login -->
-          <form action="register.php" method="POST">
+          <div id="login_form">
+            <form action="register.php" method="POST">
 
-            <!-- Login Email -->
-            <input class="input" type="email" name="login_email" placeholder="Email Address" value="
+              <!-- Login Email -->
+              <input class="input" type="email" name="login_email" placeholder="Email Address" value="
 <?php
 if (isset($_SESSION["login_email"])) {
   echo $_SESSION["login_email"];
 }
 ?>" required>
-            <br>
+              <br>
 
-            <!-- Login Password -->
-            <input class="input" type="password" name="login_password" placeholder="Password">
-            <br>
+              <!-- Login Password -->
+              <input class="input" type="password" name="login_password" placeholder="Password">
+              <br>
 
-            <!-- Login Button -->
-            <input class="button is-dark" type="submit" name="login_button" value="Login" style="margin-top: 10px;">
-          </form>
-          <br>
-          <?php if (in_array("Incorrect Email or Password<br>", $error_array)) echo "Incorrect Email or Password<br>" ?>
-          <br>
+              <!-- Login Button -->
+              <input class="button is-dark" type="submit" name="login_button" value="Login" style="margin-top: 10px;">
+            </form>
+            <br>
+            <?php if (in_array("Incorrect Email or Password<br>", $error_array)) echo "Incorrect Email or Password<br>" ?>
+            <a href="#" id="login_link" onclick="switchToRegisterForm()">New? Click here to sign up!</a>
+            <!-- i could put this script into a different file. Should I? -->
+            <script>
+              function switchToRegisterForm() {
+                document.getElementById("login_form").style.display = "none";
+                document.getElementById("register_form").style.display = "block";
+              };
+            </script>
+          </div>
+
+
+
 
           <!-- Register -->
-          <form action="register.php" method="POST">
+          <!-- Start with the signup hidden showing only the login form -->
+          <div id="register_form" style="display: none;">
+            <form action="register.php" method="POST">
 
-            <!-- first name -->
-            <input class="input" type="text" name="register_firstname" placeholder="First Name" value="
+              <!-- first name -->
+              <input class="input" type="text" name="register_firstname" placeholder="First Name" value="
 <?php
 if (isset($_SESSION["register_firstname"])) {
   echo $_SESSION["register_firstname"];
 }
 ?>" required>
-            <br>
-            <?php if (in_array("Your first name must be between 2 and 25 characters.<br>", $error_array)) echo "Your first name must be between 2 and 25 characters.<br>"; ?>
+              <br>
+              <?php if (in_array("Your first name must be between 2 and 25 characters.<br>", $error_array)) echo "Your first name must be between 2 and 25 characters.<br>"; ?>
 
-            <!-- last name -->
-            <input class="input" type="text" name="register_lastname" placeholder="Last Name" value="
+              <!-- last name -->
+              <input class="input" type="text" name="register_lastname" placeholder="Last Name" value="
 <?php
 if (isset($_SESSION["register_lastname"])) {
   echo $_SESSION["register_lastname"];
 }
 ?>" required>
-            <br>
-            <?php if (in_array("Your last name must be between 2 and 25 characters.<br>", $error_array)) echo "Your last name must be between 2 and 25 characters.<br>"; ?>
+              <br>
+              <?php if (in_array("Your last name must be between 2 and 25 characters.<br>", $error_array)) echo "Your last name must be between 2 and 25 characters.<br>"; ?>
 
-            <!-- email -->
-            <input class="input" type="email" name="register_email" placeholder="Email" value="
+              <!-- email -->
+              <input class="input" type="email" name="register_email" placeholder="Email" value="
     <?php
     if (isset($_SESSION["register_email"])) {
       echo $_SESSION["register_email"];
     }
     ?>" required>
-            <br>
+              <br>
 
-            <!-- verify email -->
-            <input class="input" type="email" name="register_email2" placeholder="Confirm Email" value="
+              <!-- verify email -->
+              <input class="input" type="email" name="register_email2" placeholder="Confirm Email" value="
     <?php
     if (isset($_SESSION["register_email2"])) {
       echo $_SESSION["register_email2"];
     }
     ?>" required>
-            <br>
-            <?php if (in_array("Email is already in use<br>", $error_array)) {
-              echo "Email is already in use<br>";
-            } else if (in_array("Invalid Email Format<br>", $error_array)) {
-              echo "Invalid Email Format<br>";
-            } else if (in_array("Emails don't match<br>", $error_array)) {
-              echo "Emails don't match<br>";
-            } ?>
+              <br>
+              <?php if (in_array("Email is already in use<br>", $error_array)) {
+                echo "Email is already in use<br>";
+              } else if (in_array("Invalid Email Format<br>", $error_array)) {
+                echo "Invalid Email Format<br>";
+              } else if (in_array("Emails don't match<br>", $error_array)) {
+                echo "Emails don't match<br>";
+              } ?>
 
-            <!-- password -->
-            <input class="input" type="password" name="register_password" placeholder="Password" required>
-            <br>
+              <!-- password -->
+              <input class="input" type="password" name="register_password" placeholder="Password" required>
+              <br>
 
-            <!-- verify password -->
-            <input class="input" type="password" name="register_password2" placeholder="Confirm Password" required>
-            <br>
-            <?php if (in_array("You password must be between 6 and 30 characters<br>", $error_array)) {
-              echo "You password must be between 6 and 30 characters<br>";
-            } else if (in_array("Password can only contain letter a-z and/or numbers<br>", $error_array)) {
-              echo "Password can only contain letter a-z and/or numbers<br>";
-            } else if (in_array("Your passwords don't match<br>", $error_array)) {
-              echo "Your passwords don't match<br>";
-            } ?>
+              <!-- verify password -->
+              <input class="input" type="password" name="register_password2" placeholder="Confirm Password" required>
+              <br>
+              <?php if (in_array("You password must be between 6 and 30 characters<br>", $error_array)) {
+                echo "You password must be between 6 and 30 characters<br>";
+              } else if (in_array("Password can only contain letter a-z and/or numbers<br>", $error_array)) {
+                echo "Password can only contain letter a-z and/or numbers<br>";
+              } else if (in_array("Your passwords don't match<br>", $error_array)) {
+                echo "Your passwords don't match<br>";
+              } ?>
 
-            <!-- submit button -->
-            <input class="button is-info" type="submit" name="register_button" value="Register" style="margin-top: 10px;">
-          </form>
+              <!-- submit button -->
+              <input class="button is-info" type="submit" name="register_button" value="Register" style="margin-top: 10px;">
+            </form>
+            <br>
+            <a href="#" id="register_link" onclick="switchToLoginForm()">Already have an account? Click here.</a>
+            <!-- i could put this script into a different file. Should I? -->
+            <script>
+              function switchToLoginForm() {
+                document.getElementById("login_form").style.display = "block";
+                document.getElementById("register_form").style.display = "none";
+              };
+            </script>
+          </div>
+
 
         </div>
       </div>
     </div>
   </section>
-  <!-- </div> -->
-  <!-- </div> -->
-  <!-- </div> -->
+
 
 </body>
 

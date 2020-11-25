66dc2aa0c3f8a239d05d381a0d37e61ded591fdb
zeroIndex0 <sagtogi@gmail.com>
2020-11-25 12:59:34 -0500

    removed connection test

--- index.php
+++ index.php
@@ -12,17 +12,6 @@
   <?php echo "hello again <br>"; ?>
   <?php echo "You are logged in as: " . $_SESSION["username"] . ".  Welcome! <br>"; ?>
 
-  
-  <form method="POST">
-    <input type="submit" name="test" value="button">
-  </form>
-  <!-- Added mock data into the database, just using as a test to check my db connection -->
-  <?php if (isset($_POST["test"])) {
-    include_once "api/read.php";
-    echo "Id: " . $data_array[0]["id"];
-    echo "<br>";
-    echo "Username: " . $data_array[0]["username"];
-  } ?>
 </body>
 
 </html>
